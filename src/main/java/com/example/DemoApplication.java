6fbf8e2702add4e8f12b4c43cce997739d99e1fa
rain <rain@192.168.1.104>
2017-02-18 18:40:36 +0800

    getAllData 接口

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,34 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.WorkbookDocument;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,9 +36,29 @@ import org.springframework.web.servlet.ModelAndView;
 @SpringBootApplication
 public class DemoApplication {
 	@RequestMapping("/")
+	@ResponseBody
 	public ModelAndView map(){
 		ModelAndView mv=new ModelAndView("index");
 		return mv;
+	}
+	@RequestMapping(value="/getAllData")
+	public List<News> readExcel(){
+		List<News> result=new ArrayList<>();
+        Sheet sheet;
+		 try {
+	            File excelFile = new File("data1.xlsx"); //创建文件对象
+	            Workbook wb=new XSSFWorkbook(new FileInputStream(excelFile));
+	            sheet =wb.getSheetAt(0);
+	            for(Row row:sheet){
+	            	if(row.getCell(0)==null||row.getCell(1)==null||row.getCell(2)==null||row.getCell(3)==null||row.getCell(4)==null||row.getCell(5)==null||row.getCell(6)==null)
+	            		continue;
+	            	result.add(new News(row.getCell(0).toString(),row.getCell(1).toString(),row.getCell(2).toString(),row.getCell(3).toString(),row.getCell(4).toString(),row.getCell(5).toString(),row.getCell(6).toString()));
+	            }
+	        }
+	        catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		return result;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/com/example/News.java
@@ -0,0 +1,62 @@
+package com.example;
+
+public class News {
+	private String source;
+	private String title;
+	private String content;
+	private String keyWord;
+	private String pageTitle;
+	private String introduction;
+	private String date;
+	News(String source,String title,String content,String keyWord,String papgeTitle,String introduction,String date){
+		this.source=source;
+		this.title=title;
+		this.content=content;
+		this.keyWord=keyWord;
+		this.pageTitle=papgeTitle;
+		this.introduction=introduction;
+		this.date=date;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getKeyWord() {
+		return keyWord;
+	}
+	public void setKeyWord(String keyWord) {
+		this.keyWord = keyWord;
+	}
+	public String getPageTitle() {
+		return pageTitle;
+	}
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
