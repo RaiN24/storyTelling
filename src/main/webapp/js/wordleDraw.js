8831aa2658183897cd9868177f14bc6e73712fba
sksf1 <sksf1@DESKTOP-K2MJTEI>
2017-05-01 20:49:24 +0800

    change wordle in the sort way

--- src/main/webapp/js/wordleDraw.js
+++ src/main/webapp/js/wordleDraw.js
@@ -1,16 +1,21 @@
+var word_number_max = 50;
 function draw_wordle(x,y,w,h,newsNumber,start,end,Xscale){
          var news = json_by_news[newsNumber];
          var words = [];
          var maxTime = 0;
          for (var i in news){
-            words.push(i);
+        	 words.push({name: i, time: news[i]});
             if(news[i]>maxTime) maxTime = news[i];
          }
-    
+         words.sort(by("time"));
+         
+         var delta = words.length - word_number_max;
+         for (var i = 0; i < delta; i++) words.pop();
+         
          var layout = d3.layout.cloud()
             .size([w, h])
             .words(words.map(function(d) {
-              return {text: d, size: 10 + news[i] * 20/ maxTime, test: "haha"};
+              return {text: d.name, size: 10 + d.time * 20 / maxTime, test: "haha"};
             }))
             .padding(5)
             .rotate(function() { return -45 + Math.random()*90; })
@@ -19,6 +24,25 @@ function draw_wordle(x,y,w,h,newsNumber,start,end,Xscale){
             .on("end", draw);
 
         layout.start();
+        function by(name){
+            return function(o, p){
+        var a, b;
+        if (typeof o === "object" && typeof p === "object" && o && p) {
+            a = o[name];
+            b = p[name];
+            if (a === b) {
+                return 0;
+            }
+            if (typeof a === typeof b) {
+                return a < b ? 1 : -1;
+            }
+            return typeof a < typeof b ? 1 : -1;
+        }
+        else {
+            throw ("error");
+        }
+    }
+}    
         function draw(words) {
         	//console.log("*");
            wordleImage =  svg_4.append("g")
